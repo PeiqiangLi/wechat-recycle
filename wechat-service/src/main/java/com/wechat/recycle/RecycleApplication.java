8984de9d39f4ccb8fe146b56a0732c35ab17a556
peiqiangli <peiqiang02@126.com>
2018-12-24 23:21:19 +0800

    first commit

---
+++ wechat-service/src/main/java/com/wechat/recycle/RecycleApplication.java
@@ -0,0 +1,14 @@
+package com.wechat.recycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecycleApplication.class, args);
+    }
+
+}
+
